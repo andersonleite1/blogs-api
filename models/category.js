be920f81a629e7905f3470df832a8bc8dff9e4e1
Anderson Leite <andersonleite.dev@gmail.com>
2022-04-28 21:20:24 -0300

    feat: model category

---
+++ models/category.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { allowNull: false, type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { allowNull: false, type: DataTypes.STRING },
+  },
+  {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+
+  return Category;
+};
