936877036ce1ecfd80c0b6e1163a25ed2c4ab14e
Anderson Leite <andersonleite.dev@gmail.com>
2022-05-02 12:33:40 -0300

    feat: index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const router = require('./routes');
+
 const app = express();
+
+app.use(express.json());
+
+app.use(
+  '/user',
+  router.user,
+);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
