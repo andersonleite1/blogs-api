ba320119b6c3999613ec5b00fbd0a86a03caa99c
Anderson Leite <andersonleite.dev@gmail.com>
2022-05-02 18:50:46 -0300

    feat: schema  add  messages case empty

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -2,13 +2,16 @@ const errors = {
   nameLength: '"displayName" length must be at least 8 characters long',
   emailInvalid: '"email" must be a valid email',
   emailBlank: '"email" is required',
+  emailEmpty: '"email" is not allowed to be empty',
   passwordLength: '"password" length must be 6 characters long',
   passwordBlank: '"password" is required',
+  passwordEmpty: '"password" is not allowed to be empty',
 };
 
 const code = 400;
 
-const blank = (value) => (!value); 
+const blank = (value) => (!value);
+const empty = (value) => (!value && typeof value === 'string'); 
 const isLengthLetterThan = (value, min) => (value.length < min);
 const isEmailValid = (email) => {
   const atSign = email.indexOf('@');
@@ -30,6 +33,7 @@ const validationsName = (name) => {
 
 const validationsEmail = async (email) => {
   switch (true) {
+    case empty(email): return { code, message: errors.emailEmpty };
     case blank(email): return { code, message: errors.emailBlank };
     case !isEmailValid(email): return { code, message: errors.emailInvalid };
     default: return {};
@@ -38,6 +42,7 @@ const validationsEmail = async (email) => {
 
 const validationsPassword = (password) => {
   switch (true) {
+    case empty(password): return { code, message: errors.passwordEmpty };
     case blank(password): return { code, message: errors.passwordBlank };
     case isLengthLetterThan(password, 6): return { code, message: errors.passwordLength };
     default: return {};
